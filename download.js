83201d1a566f6bcfe4e5189e4a9ae5bcc0af6dd5
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-05 00:33:09 +0300

    link generation

--- download.js
+++ download.js
@@ -2,23 +2,45 @@ const $dwnldBtn = document.getElementById('dwnld-btn');
 const $dwnldPopup = document.getElementById('dwnld-popup');
 const $darkBckgr = document.getElementById('dark-bckgr');
 const $dwnldCancelBtn = document.getElementById('dwnld-cancel');
-const rect = document.getElementById('rect');
-const imgWidth = document.getElementById('img-width');
-const imgHeight = document.getElementById('img-height');
+const $rect = document.getElementById('rect');
+const $imgWidth = document.getElementById('img-width');
+const $imgHeight = document.getElementById('img-height');
+
+const $gifLink = document.getElementById('gif-link'),
+    $jpgLink = document.getElementById('jpg-link'),
+    $pngLink = document.getElementById('png-link'),
+    $svgLink = document.getElementById('svg-link');
+
+refreshLinks = () => {
+    const link = `https://csscolor.ru/download/${palette.currentColor.hex}`;
+    const size = '?size=' + ($imgWidth.value === '' ? 256 : $imgWidth.value) + 'x' + ($imgHeight.value === '' ? 256 : $imgHeight.value);
+    let alpha = '';
+    if (palette.currentColor.alpha !== 1) {
+        alpha = `&alpha=${Math.round(palette.currentColor.alpha*100)/100}`
+    }
+    $gifLink.setAttribute('href', link + '.gif' + size + alpha);
+    $jpgLink.setAttribute('href', link + '.jpg' + size + alpha);
+    $pngLink.setAttribute('href', link + '.png' + size + alpha);
+    $svgLink.setAttribute('href', link + '.svg' + size + alpha);
+};
 
 $dwnldBtn.addEventListener('click', () => {
-    rect.style.background = '#' + palette.currentColor.hex;
-    rect.style.color = palette.getReadableColorOverColor();
+    $rect.style.background = '#' + palette.currentColor.hex;
+    $rect.style.color = palette.getReadableColorOverColor();
     if (palette.getReadableColorOverTone() !== '#fff') {
-        imgWidth.classList.add('dark');
-        imgHeight.classList.add('dark');
+        $imgWidth.classList.add('dark');
+        $imgHeight.classList.add('dark');
     } else {
-        imgWidth.classList.remove('dark');
-        imgHeight.classList.remove('dark');
+        $imgWidth.classList.remove('dark');
+        $imgHeight.classList.remove('dark');
     }
+    refreshLinks();
 
     $dwnldPopup.classList.add('visible');
 });
+
+$imgHeight.addEventListener('change', refreshLinks);
+$imgWidth.addEventListener('change', refreshLinks);
 
 $darkBckgr.addEventListener('click', () => {
     $dwnldPopup.classList.remove('visible');
