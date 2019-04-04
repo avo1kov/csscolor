65f8ed74b009efe61c26bbac440735222b560908
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-04 23:59:24 +0300

    auto setting text color on download popup

--- download.js
+++ download.js
@@ -9,8 +9,13 @@ const imgHeight = document.getElementById('img-height');
 $dwnldBtn.addEventListener('click', () => {
     rect.style.background = '#' + palette.currentColor.hex;
     rect.style.color = palette.getReadableColorOverColor();
-    imgWidth.style.color = palette.getReadableColorOverColor();
-    imgHeight.style.color = palette.getReadableColorOverColor();
+    if (palette.getReadableColorOverTone() !== '#fff') {
+        imgWidth.classList.add('dark');
+        imgHeight.classList.add('dark');
+    } else {
+        imgWidth.classList.remove('dark');
+        imgHeight.classList.remove('dark');
+    }
 
     $dwnldPopup.classList.add('visible');
 });
