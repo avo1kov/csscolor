e757a84fe1f8303cbef915d408f84f7e8c8ac6f1
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-04 23:56:27 +0300

    auto setting text color of "x" on download popup

--- download.js
+++ download.js
@@ -3,9 +3,15 @@ const $dwnldPopup = document.getElementById('dwnld-popup');
 const $darkBckgr = document.getElementById('dark-bckgr');
 const $dwnldCancelBtn = document.getElementById('dwnld-cancel');
 const rect = document.getElementById('rect');
+const imgWidth = document.getElementById('img-width');
+const imgHeight = document.getElementById('img-height');
 
 $dwnldBtn.addEventListener('click', () => {
     rect.style.background = '#' + palette.currentColor.hex;
+    rect.style.color = palette.getReadableColorOverColor();
+    imgWidth.style.color = palette.getReadableColorOverColor();
+    imgHeight.style.color = palette.getReadableColorOverColor();
+
     $dwnldPopup.classList.add('visible');
 });
 
