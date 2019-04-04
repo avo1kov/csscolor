818eb74291606e82ded4182ec6580a1cf5b55b27
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-04 23:54:03 +0300

    chosen color shows on download popup

--- download.js
+++ download.js
@@ -1,9 +1,11 @@
 const $dwnldBtn = document.getElementById('dwnld-btn');
 const $dwnldPopup = document.getElementById('dwnld-popup');
 const $darkBckgr = document.getElementById('dark-bckgr');
-const $dwnldCancelBtn = document.getElementById('dwnld-cancel')
+const $dwnldCancelBtn = document.getElementById('dwnld-cancel');
+const rect = document.getElementById('rect');
 
 $dwnldBtn.addEventListener('click', () => {
+    rect.style.background = '#' + palette.currentColor.hex;
     $dwnldPopup.classList.add('visible');
 });
 
