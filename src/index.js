00cbafd1492e71fa58d4046e168ccaed0e72b79c
Alexander Volkov <avo1kov@MacBook-Pro-Alexander.local>
2020-06-20 21:45:02 +0300

    updated font for mac and minor improvements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/default.sass';
 import './styles/donate.scss';
 import './styles/download.sass';
 import './styles/paint.scss';
+import './styles/comments.scss';
 // import './styles/color_sets.scss';
 
 import './scripts/script.js';
