cf7603c9db2758a93bc1501c96f23e4dbff6c462
art <art@MacBook-Air-art.local>
2024-11-05 11:04:22 +0300

    changed tshirts-button wrapper from div to a

--- src/scripts/tshirts.js
+++ src/scripts/tshirts.js
@@ -1,5 +1,5 @@
 const $tshirtsBtn = document.getElementById('tshirts_btn');
 
 $tshirtsBtn.addEventListener('click', () => {
-    window.open('https://onlytone.ru/items?ref=csscolor&color=' + palette.currentColor.hex.substring(0,6), '_blank');
+    $tshirtsBtn.href = 'https://onlytone.ru/items?ref=csscolor&color=' + palette.currentColor.hex.substring(0,6);
 });
